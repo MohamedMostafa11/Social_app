900986a9748c8b753e3c31dd7291e00f651710d2
Mohamed Mostafa <@MohamedMostafa11>
2023-11-20 00:49:11 +0200

    bbb

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:social_app/app.dart';
+
+import 'package:social_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
