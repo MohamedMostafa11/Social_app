d6f9ca89c39e72e8a14b44f39f0c89fbcbe95723
Abdelrohman ali <93785413+Abdelrohmanaliabas@users.noreply.github.com>
2023-11-21 00:13:36 +0200

    Abas

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/scheduler.dart';
 import 'package:social_app/app.dart';
 import 'package:social_app/theme/theme_helper.dart';
 import 'package:social_app/routes/app_routes.dart';
-
+//sadasdcas
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
