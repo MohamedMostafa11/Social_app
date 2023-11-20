081a79a50b95fd0d0169c23e7f7e4c8635a8cb5d
Abdelrahman <zeid4643@gmail.com>
2023-11-21 00:24:43 +0200

    abas said

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:social_app/app.dart';
 import 'package:social_app/theme/theme_helper.dart';
-import 'package:social_app/routes/app_routes.dart';
-
+//kmklmmk;;m
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
   ]);
   ThemeHelper().changeTheme('primary');
-  runApp(MyApp());
+  runApp(
+    MyApp(),
+  );
 }
+
+
+
+
