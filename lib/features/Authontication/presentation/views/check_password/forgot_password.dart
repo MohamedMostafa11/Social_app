b15809700dc6476adc2d5eb2bea6d36d84039663
Mohamed Mostafa <@MohamedMostafa11>
2023-11-23 04:05:45 +0200

    add Verify code screen

---
+++ lib/features/Authontication/presentation/views/check_password/forgot_password.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class forgotPasswordScreen extends StatelessWidget {
+  const forgotPasswordScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/features/Authontication/presentation/views/sign_in_screen/sign_in_screen.dart
+++ lib/features/Authontication/presentation/views/sign_in_screen/sign_in_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:social_app/core/app_export.dart';
 import 'package:social_app/core/utils/app_functions.dart';
+import 'package:social_app/features/Authontication/presentation/views/check_password/verify_screen.dart';
 import 'package:social_app/features/Authontication/presentation/views/sign_up_screen/sign_up_screen.dart';
 import 'package:social_app/features/splash/presentation/views/boarding_screen/boarding_screen.dart';
 import 'package:social_app/widgets/custom_elevated_button.dart';
@@ -154,7 +155,7 @@ class SignInScreen extends StatelessWidget {
 
   /// Navigates to the forgotPasswordScreen when the action is triggered.
   onTapTxtFORGOTPASSWORD(BuildContext context) {
-    Navigation(context: context, next: BoardingScreen());
+    Navigation(context: context, next: VerifyScreen());
   }
 
   /// Navigates to the signUpScreen when the action is triggered.
